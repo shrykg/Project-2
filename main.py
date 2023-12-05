8fdf68544ea417272d8431c53f9452163bc75f58
Shreyak Godala <shreyakgodala2909@gmail.com>
2023-12-05 14:10:17 -0500

    implemented main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import GameEngine
+def main():
+    gameEngine = GameEngine()
+    gameEngine.initializeGame()
+    gameEngine.intro()
+    veggiesRemaining = gameEngine.remainingVeggies()
+
+    while veggiesRemaining > 0:
+        print(f'{veggiesRemaining} veggies remaining. Current score: {gameEngine.getScore()}')
+        gameEngine.printField()
+        gameEngine.moveRabbits()
+        gameEngine.moveCaptain()
+        veggiesRemaining = gameEngine.remainingVeggies()
+
+    gameEngine.gameOver()
+    gameEngine.highScore()
+
+main()
